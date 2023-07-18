329e8bf7a9fe9483f7b4476f8c7f0bd299d831d4
adminuser <adminuser@kali.mvm>
2023-07-18 07:33:38 +0530

    updated the base_config file

--- base_config.py
+++ base_config.py
@@ -7,7 +7,7 @@ import numpy as np
 def get_baseconfig(model_type="mixer_l16_224.goog_in21k_ft_in1k", 
                   image_size=224, 
                   patch_size=16, 
-                  depths=24,
+                  depth=24,
                   projection_dim=1024,
                   mlp_ratio=(0.5, 4.0),
                   init_values=None,
@@ -24,7 +24,7 @@ def get_baseconfig(model_type="mixer_l16_224.goog_in21k_ft_in1k",
     config.patch_size = patch_size
     config.image_size = image_size
     config.num_patches = pow(config.image_size // config.patch_size, 2)
-    config.depths = depths
+    config.depth = depth
     config.projection_dim = projection_dim
     config.input_shape = (config.image_size, config.image_size, 3)
     config.init_values = init_values
