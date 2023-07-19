0d06a828680863bb1db61ee39e14b0b1d8e1c11a
adminuser <adminuser@kali.mvm>
2023-07-19 17:47:52 +0530

    added code and configs for gated mlp

--- mlp_mixers/block_layers.py
+++ mlp_mixers/block_layers.py
@@ -2,6 +2,6 @@ from .blocks import MixerBlock, SpatialGatingBlock
 
 
 BLOCK_LAYERS = {
-    "mixer_block": MixerBlock
+    "mixer_block": MixerBlock,
     "spatial_gating_block": SpatialGatingBlock
  }
