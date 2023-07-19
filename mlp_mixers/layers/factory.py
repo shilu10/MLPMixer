dede38346e7ee3ab3747802cb2940978f77b4854
Shilu <shilu842003@gmail.com>
2023-07-19 18:38:53 +0530

    added resblock code

--- mlp_mixers/layers/factory.py
+++ mlp_mixers/layers/factory.py
@@ -58,3 +58,33 @@ def norm_layer_factory(norm_layer: str):
 
     else:
         raise ValueError(f"Unknown normalization layer: {norm_layer}")
+
+
+class Affine(tf.keras.layers.Layer):
+    """
+    Affine normalisation as used in ResMLP networks.
+
+    For NHWC ``x``, we return ``alpha * x + beta``, where ``alpha``, ``beta`` are C
+    tensors.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.alpha = self.add_weight(
+            shape=(input_shape[-1],),
+            initializer="ones",
+            trainable=True,
+            name="alpha",
+        )
+        self.beta = self.add_weight(
+            shape=(input_shape[-1],),
+            initializer="zeros",
+            trainable=True,
+            name="beta",
+        )
+
+    def call(self, x):
+        x = self.alpha * x + self.beta
+        return x
