653634b7157d1e203b6435e85813b6c37e705544
Shilu <shilu842003@gmail.com>
2023-07-21 06:49:15 +0530

    updated the conver_all code

--- convert_all.py
+++ convert_all.py
@@ -9,24 +9,35 @@ from .convert import port
 all_model_types = [
     "mixer_l16_224.goog_in21k_ft_in1k",
     "mixer_b16_224_goog_in21k_ft_in1k",
+    "gmixer_12_224",
+    "gmixer_24_224",
+    "gmlp_b16_224",
+    "gmlp_s16_224",
+    "gmlp_ti16_224",
+    "resmlp_12_224",
+    "resmlp_24_224"
 ]
 
 def port_all(mtype="classifier", model_savepath="models/"):
-    if mtype == "classifier":
-        for model_type in all_model_types:
-            print("Processing model type: ", model_type)
-            port(
-                model_type = model_type,
-                model_savepath = model_savepath,
-                include_top = True
-            )
+    try: 
+        if mtype == "classifier":
+            for model_type in all_model_types:
+                print("Processing model type: ", model_type)
+                port(
+                    model_type = model_type,
+                    model_savepath = model_savepath,
+                    include_top = True
+                )
+        
+        elif mtype == "feature_extractor":
+            for model_type in all_model_types:
+                print("Processing model type: ", model_type)
+                port(
+                    model_type = model_type,
+                    model_savepath = ".",
+                    include_top = False
+                )
     
-    elif mtype == "feature_extractor":
-        for model_type in all_model_types:
-            print("Processing model type: ", model_type)
-            port(
-                model_type = model_type,
-                model_savepath = ".",
-                include_top = False
-            )
+    except Exception as err:
+        return err
                 
