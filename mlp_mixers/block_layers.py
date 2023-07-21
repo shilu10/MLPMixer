e5805758a17516ec38b4bb859c8188180225de18
adminuser <adminuser@kali.mvm>
2023-07-21 06:45:05 +0530

    updated block_layers dict

--- mlp_mixers/block_layers.py
+++ mlp_mixers/block_layers.py
@@ -1,7 +1,8 @@
-from .blocks import MixerBlock, SpatialGatingBlock
+from .blocks import MixerBlock, SpatialGatingBlock, ResBlock
 
 
 BLOCK_LAYERS = {
     "mixer_block": MixerBlock,
-    "spatial_gating_block": SpatialGatingBlock
+    "spatial_gating_block": SpatialGatingBlock,
+    "res_block": ResBlock
  }
